015623149000a91c051562b766c3d708a45ce0f0
Nico Linder <nico@nicoviii.de>
2018-07-04 18:32:45 +0200

    try to make ready for first integration

---
+++ CompleteInformation.Core/FSharp/Application.fs
@@ -0,0 +1,22 @@
+module CompleteInformation.Core.FSharp.Application
+
+open Couchbase.Lite
+
+type T = {
+    databases: Map<string, Database>;
+    extensions: Extension.T list;
+}
+
+type Setup = {
+    databases: string list;
+    extensions: string list;
+}
+
+let createSetup databases extensions =
+    { Setup.databases = databases; extensions = extensions }
+
+let setUpApplication (setup:Setup) =
+    let databases =
+        setup.databases
+        |> List.fold (fun (map:Map<string,Database>) d -> map.Add(d, new Database (d))) Map.empty
+    { T.databases=databases; extensions=[] }
