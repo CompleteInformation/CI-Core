2aff1f7ee03d310fee31958d3b6f59391acea901
Nico Linder <nico@myboshi.net>
2018-07-13 14:46:41 +0200

    add open statement

--- CompleteInformation.Core/FSharp/Application.fs
+++ CompleteInformation.Core/FSharp/Application.fs
@@ -1,5 +1,7 @@
 module CompleteInformation.Core.FSharp.Application
 
+open Couchbase.Lite
+
 type T = {
     databases: Map<string, Couchbase.Lite.Database>;
     extensions: Extension.T list;
@@ -17,5 +19,5 @@ let setUpApplication (setup:Setup) =
     Couchbase.Lite.Support.NetDesktop.Activate()
     let databases =
         setup.databases
-        |> List.fold (fun (map:Map<string,Couchbase.Lite.Database>) d -> map.Add(d, new Couchbase.Lite.Database (d))) Map.empty
+        |> List.fold (fun (map:Map<string,Database>) d -> map.Add(d, new Database (d))) Map.empty
     { T.databases=databases; extensions=[] }
